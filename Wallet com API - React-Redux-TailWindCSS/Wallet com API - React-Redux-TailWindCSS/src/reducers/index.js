8069990130099a256a848c2e93b66f8d01b9ef4e
PedroMarqDev <PedroMarqDev@gmail.com>
2021-01-23 20:03:52 -0300

    Vivendo e aprendendo a usar o git

--- Wallet com API - React-Redux-TailWindCSS/Wallet com API - React-Redux-TailWindCSS/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import userReducer from './user';
-import walletReducer from './wallet';
-
-const reducers = combineReducers({ user: userReducer, wallet: walletReducer });
-
-export default reducers;
